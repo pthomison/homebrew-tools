7861c6e96b32ad62d0d0f0b987b732663976e367
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-04 13:21:07 -0700

    Brew formula update for go-helloworld version 0.0.4

--- go-helloworld.rb
+++ go-helloworld.rb
@@ -5,20 +5,20 @@
 class GoHelloworld < Formula
   desc ""
   homepage ""
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/pthomison/go-helloworld/releases/download/0.0.3/go-helloworld_0.0.3_Darwin_x86_64.tar.gz"
-    sha256 "86b23689d062f8e9b2b7ff18a5db1e20ad270984bd2b5a8513e8f7a3cee0a98c"
+    url "https://github.com/pthomison/go-helloworld/releases/download/0.0.4/go-helloworld_0.0.4_Darwin_x86_64.tar.gz"
+    sha256 "b063aa5c8e60d88a8ca6fecace40c975b381124acbf5f87225b024c72afa453d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/pthomison/go-helloworld/releases/download/0.0.3/go-helloworld_0.0.3_Linux_x86_64.tar.gz"
-    sha256 "a1fc065c8bf0d864686bfb22ea6014508c91f58100376adbd8095e48e5d133a6"
+    url "https://github.com/pthomison/go-helloworld/releases/download/0.0.4/go-helloworld_0.0.4_Linux_x86_64.tar.gz"
+    sha256 "3cb9aa5fed1f938e65faeb7e414e45af6a1f397b6c18e24c346dc7bb80f0f93e"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/pthomison/go-helloworld/releases/download/0.0.3/go-helloworld_0.0.3_Linux_arm64.tar.gz"
-    sha256 "34cf0c350635d9e30977ab8b3e16d696966664c2dab8c678e4768e78a148d27c"
+    url "https://github.com/pthomison/go-helloworld/releases/download/0.0.4/go-helloworld_0.0.4_Linux_arm64.tar.gz"
+    sha256 "633d94a9989be39a1a32bb006281322f74cab5358799cc7aac934299820829d7"
   end
 
   def install
